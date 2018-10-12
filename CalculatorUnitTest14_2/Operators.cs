a29de3298425a16286b4fc35e8e3e8320cf0e47b
JaneWB <brownjannick@att.net>
2018-10-12 14:43:20 -0500

    CG 14-2 CalculatorTest

---
+++ CalculatorUnitTest14_2/Operators.cs
@@ -0,0 +1,49 @@
+﻿namespace CalculatorUnitTest14_2
+{
+    internal class Operators
+    {
+        /// <summary>
+        /// Multiplies 2 numbers
+        /// </summary>
+        /// <param name="num1">first number to be multiplied</param>
+        /// <param name="num2">second number to be multiplied</param>
+        /// <returns>num1 multiplied by num2</returns>
+        public static double Multiply(double num1, double num2)
+        {
+            return (num1 * num2);
+        }
+
+        /// <summary>
+        /// Divides 2 numbers
+        /// </summary>
+        /// <param name="num1"></param>
+        /// <param name="num2"></param>
+        /// <returns>num1 is divided by num2</returns>
+        public static double Divide(double num1, double num2)
+        {
+            return (num1 / num2);
+        }
+
+        /// <summary>
+        /// Adds 2 numbers
+        /// </summary>
+        /// <param name="num1"></param>
+        /// <param name="num2"></param>
+        /// <returns>num1 is added to num2</returns>
+        public static double Addition(double num1, double num2)
+        {
+            return (num1 + num2);
+        }
+
+        /// <summary>
+        /// Subtracts num2 from num1
+        /// </summary>
+        /// <param name="num1"></param>
+        /// <param name="num2"></param>
+        /// <returns>num2 is subtracted from num1</returns>
+        public static double Minus(double num1, double num2)
+        {
+            return (num1 - num2);
+        }
+    }
+}
--- CalculatorUnitTest14_2/Program.cs
+++ CalculatorUnitTest14_2/Program.cs
@@ -2,11 +2,70 @@
 
 namespace CalculatorUnitTest14_2
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //try
+            //{
+                 //Program  for the variables first number, second number, answer and operation.  Using double allows for fractions.
+                double firstnumber = 0;
+                double secondnumber = 0;
+                double answer = 0;
+                string operation = " ";
+
+                //Ask the user to input the first number.
+                Console.WriteLine("Welcome to a simple calculator. Please enter a simple math calculation " +
+                    "using (/, x, +, -). Write the calculation as: number, space, operator, space number, IE: 2 + 2");
+                //Print the user input to the console.
+
+                string input = Console.ReadLine();
+
+                var parts = input.Split(" ");
+
+                if (parts != null)
+                {
+                    firstnumber = Convert.ToDouble(parts[0]);
+                    operation = parts[1];
+                    secondnumber = Convert.ToDouble(parts[2]);
+                }
+                //Checks if the variable to the cases, if not the default is selected. Break has to be used to exit the curly brackets.
+                switch (operation)
+                {
+                    case "x":
+                        answer = Operators.Multiply(firstnumber, secondnumber);
+                        Console.WriteLine(firstnumber + " x " + secondnumber + " = " + answer);
+                        break;
+
+                    case "/":
+                        answer = Operators.Divide(firstnumber, secondnumber);
+                        Console.WriteLine(firstnumber + " / " + secondnumber + " = " + answer);
+                        if (secondnumber == 0)
+                        {
+                            Console.WriteLine("Cannot divide by 0, please try again.");
+                        }
+                        break;
+
+                    case "+":
+                        answer = Operators.Addition(firstnumber, secondnumber);
+                        Console.WriteLine(firstnumber + " + " + secondnumber + " = " + answer);
+                        break;
+
+                    case "-":
+                        answer = Operators.Minus(firstnumber, secondnumber);
+                        Console.WriteLine(firstnumber + " - " + secondnumber + " = " + answer);
+                        break;
+
+                    default:
+                        Console.WriteLine("Invalid operator");
+                        break;
+                }
+            //}
+            //catch (Exception ex)
+            //{
+            //    Console.WriteLine("Incorrect format.  Must be number, operator, number with spaces in between each.");
+            //}
+            Console.ReadLine();
         }
     }
 }
